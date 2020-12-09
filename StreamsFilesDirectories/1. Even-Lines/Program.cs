4dab4d1f9eeb06d31e70bce5b82b1ed4082e1f0d
Milena Alexieva <doycheva_m@abv.bg>
2020-12-09 22:16:55 +0200

    Add files via upload

---
+++ StreamsFilesDirectories/1. Even-Lines/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace EvenLines
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+            using (StreamReader reader = new StreamReader($"../../../text.txt"))
+            {
+                
+                int counter = 0;
+                string output= reader.ReadLine();
+                char[] charsForReplacement = new char[] {'-', ',', '.', '!', '?'};
+                while (output != null)
+                {
+                    if (counter % 2 == 0)
+                    {
+                        Regex pattern = new Regex("[-,.!?]");
+                        output = pattern.Replace(output,"@");
+                        string[] lineArr = output.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                        for (int i = lineArr.Length-1; i>=0; i--)
+                        {
+                            Console.Write($"{lineArr[i]} ");
+                        }
+                        Console.WriteLine();
+                    }
+                    output = reader.ReadLine();
+                    counter++;
+                }
+
+            }
+        }
+    }
+}
