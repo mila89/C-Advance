946de0e5952c68facf709f4e48c809e3e4b374f2
Milena Alexieva <doycheva_m@abv.bg>
2020-12-10 20:51:27 +0200

    Add files via upload

---
+++ IteratorsandComparators/ListyIterator/ListyIterator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListyIterator
+{
+    public class ListyIterator<T>
+    {
+        private List<T> items;
+        public ListyIterator()
+        {
+            items = new List<T>();
+        }
+        public ListyIterator(List<T> list)
+        {
+            this.items = new List<T>(list);
+            Index = 0;
+        }
+
+        public T CurrentItem { get; set; }
+        public int Index { get; set; }
+        public int Count 
+        { get=>items.Count;  }
+        public bool Move()
+        {
+            if (HasNext())
+            {
+                this.Index++;
+                return true;
+            }
+            return false;
+        }
+
+        public bool HasNext()
+        {
+            if (this.Index+1 >= this.items.Count)
+                return false;
+            return true;
+        }
+
+        public void Print()
+        {
+            try
+            {
+                Console.WriteLine(items[this.Index]);
+            }
+            catch 
+            {
+                Console.WriteLine("Invalid Operation!");
+            }
+        }
+    }
+}
---
+++ IteratorsandComparators/ListyIterator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListyIterator
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            List<string> inputList = new List<string>();
+            for (int i = 1; i < input.Length; i++)
+            {
+                inputList.Add(input[i]);
+            }
+            ListyIterator<string> list;
+            if (inputList.Count == 0)
+                list = new ListyIterator<string>();
+            else
+                 list = new ListyIterator<string>(inputList);
+            input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            while (input[0]!="END")
+            {
+                if (input[0] == "Move")
+                {
+                    Console.WriteLine(list.Move()); 
+                }
+                else if (input[0] == "HasNext")
+                {
+                    Console.WriteLine(list.HasNext()); 
+                }
+                else if (input[0] == "Print")
+                    list.Print();
+                input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            }
+        }
+    }
+}
