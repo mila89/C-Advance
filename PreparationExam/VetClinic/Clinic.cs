6e4c0d217e47fb775c3d663cb2019e0de42f453c
Milena Alexieva <doycheva_m@abv.bg>
2020-12-13 19:13:19 +0200

    Add files via upload

---
+++ PreparationExam/VetClinic/Clinic.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VetClinic
+{
+    class Clinic
+    {
+        private List<Pet> data;
+        public Clinic(int capacity)
+        {
+            this.Capacity = capacity;
+            data = new List<Pet>();
+        }
+
+        public int Count { get=> data.Count; }
+        public int Capacity { get; set; }
+
+        public void Add(Pet currentPet)
+        {
+            if (data.Count < this.Capacity)
+                data.Add(currentPet);
+        }
+
+        public string GetStatistics()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"The clinic has the following patients:");
+            foreach (var item in data)
+            {
+                sb.AppendLine($"Pet {item.Name} with owner: {item.Owner}");
+            }
+            return sb.ToString();
+        }
+        public bool Remove(string name)
+        {
+            Pet foundedPet = PetExist(name);
+            if (foundedPet != null)
+            {
+                data.Remove(foundedPet);
+                return true;
+            }
+            return false;
+        }
+
+        public Pet GetPet(string name, string owner)
+        {
+            for (int i = 0; i < data.Count; i++)
+            {
+                if (data[i].Name == name && data[i].Owner==owner)
+                    return data[i];
+            }
+            return null;
+        }
+
+        public Pet GetOldestPet()
+        {
+            return data.OrderByDescending(x => x.Age).FirstOrDefault();
+        }
+        private Pet PetExist(string name)
+        {
+            for (int i = 0; i < data.Count; i++)
+            {
+                if (data[i].Name == name)
+                    return data[i];
+            }
+            return null;
+        }
+    }
+}
